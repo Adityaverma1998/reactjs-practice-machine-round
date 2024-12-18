9db42786fe81a41850be71170144e1775bb60915
Adityaverma1998 <vermaaditya860@gmail.com>
2024-12-18 20:17:25 +0530

    add: basic api intregration task

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Debouncing from './components/deboucing';
 import CountDown from './components/count-down';
 import LoginForm from './components/login-from';
+import ApiIntregration from './components/api-intregration';
 function App() {
   return (
     <div className="App">
      {/* <Debouncing/> */}
      {/* <CountDown/> */}
-     <LoginForm/>
+     {/* <LoginForm/> */}
+     <ApiIntregration/>
 
     </div>
   );
---
+++ src/components/api-intregration/index.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const ApiIntregration = ()=>{
+
+    const url = "https://fakestoreapi.com/products";
+    const [products,setProducts] = useState();
+    const [error,setError] = useState();
+    const [loading,setLoading] = useState(false);
+    
+    useEffect(()=>{
+        (async()=>{
+            setLoading(true);
+             try {
+                const result = await fetch(url); 
+                if(result.ok){
+                    const data = await result.json();
+                    setProducts(data)
+                } 
+             } catch (error) {
+                setLoading(false);
+                setError(error);
+             }
+
+        })()
+    },[url])
+
+    console.log("check products are",products)
+
+    return(
+        <>
+         <h1>Hello Products here </h1>
+        </>
+    )
+}
+export default ApiIntregration;
