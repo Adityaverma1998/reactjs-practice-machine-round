3a2e625008b74ff7e0de4ef971b8a8966d6ca4c5
Adityaverma1998 <vermaaditya860@gmail.com>
2024-12-21 22:41:04 +0530

    fix: fix starting to the end

--- src/components/progress-bar/index.jsx
+++ src/components/progress-bar/index.jsx
@@ -2,11 +2,33 @@ import { useEffect, useState } from 'react';
 import './style.css';
 
 const Bar = ({value = 0})=>{
-    const [percentage,setPercentage] = useState(value);
+    const [percentage,setPercentage] = useState(0);
 
     useEffect(()=>{
        setPercentage(Math.min(100,Math.max(value,0)))
     },[value])
+
+    // Math.min(100,Math.max(percentage+1,0))
+
+    useEffect(()=>{
+    
+        const interval = setInterval(()=>{
+             
+            setPercentage((prev) => {
+                if (prev < value) {
+                  return prev + 1;
+                } else if (prev > value) {
+                  return prev - 1; 
+                } else {
+                  clearInterval(interval);
+                  return prev;
+                }
+              });
+               
+  
+        },300)
+        return ()=> clearInterval(interval);
+    },[]);
     return(
        
         <div className='progress'>
